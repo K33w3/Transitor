f14c035f55c09a9cf29e2e2bb2b4cf37a0b769d0
Nils <nilsmartenson@icloud.com>
2024-06-13 19:06:30 +0200

    json file parsing

--- src/main/java/com/bcs05/Main.java
+++ src/main/java/com/bcs05/Main.java
@@ -1,10 +1,6 @@
 package com.bcs05;
 
-import java.io.IOException;
-
 import javax.swing.JFrame;
-
-import com.bcs05.data.GeoJSONReader;
 import com.bcs05.visualization.UI;
 
 public final class Main {
@@ -12,12 +8,6 @@ public final class Main {
     }
 
     public static void main(String[] args) {
-        try {
-            String geoJsonContent = GeoJSONReader.readFile("src/main/resources/amenity.geojson");
-            System.out.println(geoJsonContent);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         UI ui = new UI();
         ui.setExtendedState(JFrame.MAXIMIZED_BOTH);
         ui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);

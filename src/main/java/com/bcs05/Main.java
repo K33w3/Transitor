7bdb335ac014014f42df272169459f37f5cc9740
Nils <nilsmartenson@icloud.com>
2024-06-17 17:34:45 +0200

    fixed main

--- src/main/java/com/bcs05/Main.java
+++ src/main/java/com/bcs05/Main.java
@@ -1,8 +1,7 @@
 package com.bcs05;
 
 import javax.swing.JFrame;
-import java.util.ArrayList;
-import com.bcs05.util.jsonCoordinateArray;
+
 import com.bcs05.visualization.UI;
 
 public final class Main {
@@ -10,8 +9,6 @@ public final class Main {
     }
 
     public static void main(String[] args) {
-        ArrayList<ArrayList<String>> list = jsonCoordinateArray.createCoordinateArray();
-        System.out.println(list);
         UI ui = new UI();
         ui.setExtendedState(JFrame.MAXIMIZED_BOTH);
         ui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);

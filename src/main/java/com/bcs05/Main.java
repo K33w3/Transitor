30c3ca2e7a127c189711492b7313d6529bf7e19d
Nils <nilsmartenson@icloud.com>
2024-06-20 16:19:55 +0200

    fix

--- src/main/java/com/bcs05/Main.java
+++ src/main/java/com/bcs05/Main.java
@@ -3,14 +3,12 @@ package com.bcs05;
 import javax.swing.JFrame;
 
 import com.bcs05.visualization.UI;
-import com.bcs05.util.JSONAccessabilityScores;
 
 public final class Main {
     private Main() {
     }
 
     public static void main(String[] args) {
-        JSONAccessabilityScores.writeToCSVFile();
         UI ui = new UI();
         ui.setExtendedState(JFrame.MAXIMIZED_BOTH);
         ui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
